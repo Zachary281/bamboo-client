9d3a9aac304481ef57e6b60aab92c33f2f178f4e
Zachary281 <147658834+Zachary281@users.noreply.github.com>
2024-10-03 09:44:38 -0400

    Update Main.java
    sigma

--- launch/src/main/java/meteordevelopment/meteorclient/Main.java
+++ launch/src/main/java/meteordevelopment/meteorclient/Main.java
@@ -3,7 +3,7 @@
  * Copyright (c) Meteor Development.
  */
 
-package meteordevelopment.meteorclient;
+package bamboodevelopment.bambooclient;
 
 import javax.swing.*;
 import java.io.File;
@@ -20,8 +20,8 @@ public class Main {
 
         int option = JOptionPane.showOptionDialog(
                 null,
-                "To install Meteor Client you need to put it in your mods folder and run Fabric for latest Minecraft version.",
-                "Meteor Client",
+                "To install Bamboo Client you need to put it in your mods folder and run Fabric for latest Minecraft version.",
+                "Bamboo Client",
                 JOptionPane.YES_NO_OPTION,
                 JOptionPane.ERROR_MESSAGE,
                 null,
